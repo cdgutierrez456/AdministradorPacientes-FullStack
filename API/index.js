aea1a4701c5985fa0fa12d42a1bbcb707de4681c
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-27 15:50:22 -0500

    Conectando a base de datos en MongoDB

--- API/index.js
+++ API/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Creando el servidor
 const app = express();
+
+// Conectar a mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
 
 // Asignando puerto y arrancando el servidor
 app.listen(4000, () => {
