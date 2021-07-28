68fd09ba45b893018c65e86ca2ff4e69de1bd4d1
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-27 23:09:52 -0500

    Insertando registros en base de datos

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,8 +1,17 @@
+const Paciente = require('../models/Paciente');
 
 // Cuando se crea un nuevo paciente
-exports.nuevoCliente = (req, res, next) => {
-    // TODO: Insertar en la base de datos
-    console.log(req.body);
+exports.nuevoCliente = async (req, res, next) => {
 
-    res.json({mensaje: 'El cliente se agregó correctamente'});
+    // Crear objeto de paciente con datos de req.body
+    const paciente = new Paciente(req.body);
+
+    try {
+        await paciente.save();
+        res.json({mensaje: 'El cliente se agregó correctamente'});
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+
 }
